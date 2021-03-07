6ff3dbfc148cbc8f9a4fd5106f055040748dff84
NadaFraihat <79834098+NadaFraihat@users.noreply.github.com>
2021-03-07 18:33:07 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+alert ("Welcome to my dynamic page")
+
+var yourMail = prompt ("enter your mail");
+var yourName = prompt ("enter your name")
+if (yourMail == "nada997fraihat@gmail.com" && yourName =='Nada'){
+    
+    document.write("<h2>Hello "+yourName+"!"+"</h2><p style='color:red'>Have a nice day!</p>");
+
+
+}
+else {
+    alert("Error! please, check your email");
+    document.write("<img src='133-1331440_free-vector-graphic-create-an-account-icon-clipart.png' width=300px>");
+}
